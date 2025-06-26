fc7032495e765f33c9c1ae99b1e260140a7e9506
dj 2828 <dj28partystudio@gmail.com>
2025-06-26 12:54:10 +0200

    a

--- download mod/mod-vanilla-2/AGGIORNA-AGGIORNAMENTO.py
+++ download mod/mod-vanilla-2/AGGIORNA-AGGIORNAMENTO.py
@@ -3,23 +3,20 @@ import zipfile
 import os
 
 GITHUB = 'https://raw.githubusercontent.com/dj2828/Ultra_Vanilla_2/master/download%20mod/down/'
-COSE = ['down.py', 'leggimi.txt', 'dom per chi ha aTlauncher.py', 'mod', 'py.bat', 'se non worka.bat', 'AGGIORNA-AGGIORNAMENTO.py']
+COSE = ['down.py', 'leggimi.txt', 'mod per chi ha aTlauncher.py', 'mod.py', 'py.bat', 'se non worka.bat', 'AGGIORNA-AGGIORNAMENTO.py']
 
-def download_and_extract_zip(url):
-    response = requests.get(url)
-    zip_path = 'temp.zip'
-    
-    with open(zip_path, 'wb') as f:
-        f.write(response.content)
-    
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall('./')
-    
-    os.remove(zip_path)
+
 
 for i in COSE:
     os.remove(i)
 
-download_and_extract_zip(GITHUB + 'mod-vanilla-2.zip')
+response = requests.get(GITHUB + 'mod-vanilla-2.zip')
+zip_path = 'temp.zip'
 
-os.remove('AGGIORNA-AGGIORNAMENTO.py')
+with open(zip_path, 'wb') as f:
+    f.write(response.content)
+
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall('./')
+
+os.remove(zip_path)
