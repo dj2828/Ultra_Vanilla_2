676c62be4e52cc5cd66bd3890bf53cdf6ddef9f8
dj 2828 <dj28partystudio@gmail.com>
2025-10-15 17:41:06 +0200

    Create a.py

---
+++ server per video/a.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for, flash, jsonify
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 404
+
+@app.route('/intro')
+def intro():
+    return send_from_directory('C:/Users/dj28p/Desktop/UV2/', 'Intro Vanilla 2-yt.mp4')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
